12d9481eff852d860b66f9a58861924d4098c324
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2020-10-17 14:56:32 +0530

    Update auto_copy.bat

--- auto_copy.bat
+++ auto_copy.bat
@@ -4,23 +4,31 @@ title Auto Copy
 echo Copying Files....
 echo -----------------
 
-xcopy "D:\source\*.cfg" "D:\destination1" /Y /I /Q
+:: IMPORTANT:- `exclude_list.txt` should be in the same directory where your auto_copy.bat file is.
+xcopy "D:\source\*.cfg" "D:\destination1" /Y /I /Q /EXCLUDE:exclude_list.txt
 xcopy "D:\source\*.cfg" "D:\destination2" /Y /I /Q
 xcopy "D:\source\*.docx" "D:\destination3" /Y /I /Q
 
-REM SYNTAX: xcopy "SOURCE_FOLDER\*.cfg" "DESTINATION_FOLDER" /Y /I/ /Q
-REM You can also add /EXCLUDE:_exclude_list.txt        // But you need to make a file called `_exclude_list.txt` and write `.txt` and `.exe` inside it if you want to exclude all *.txt & *.exe files. Even if any file/folder which contains `.txt` & `.exe` in its name will also be excluded if it exists in SOURCE_FOLDER.
-REM /Y = Will not prompt to replace already existing file at DESTINATION_FOLDER.
-REM /I = If specified DESTINATION_FOLDER doesn't exist then it will create with the name specified in this script.
-REM /Q = Suppresses the display of xcopy messages.
-REM /S = Copies directories and subdirectories, unless they are empty. If you omit /s, xcopy works within a single directory.
-REM /E = Copies all subdirectories, even if they are empty. Use /e with the /s and /t command-line options.
-REM /T = Copies the subdirectory structure (that is, the tree) only, not files. To copy empty directories, you must include the /e command-line option.
-REM /H = Copies files with hidden and system file attributes. By default, xcopy does not copy hidden or system files.
-REM /K = Copies files and retains the read-only attribute on Destination files if present on the Source files. By default, xcopy removes the read-only attribute.
-REM xcopy DOCUMENTATION:- https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/xcopy
+:: Using `for` loop to copy multiple file types from same source without writing separate lines of codes.
+for %%e in (docx pdf wri txt pptx xlsx cfg) do (
+xcopy "C:\source\*.%%e" "D:\_Destination" /Y /I /Q /EXCLUDE:exclude_list.txt
+xcopy "C:\source\*.%%e" "E:\destination" /Y /I /Q /EXCLUDE:exclude_list.txt > D:\database\List_of_files_copied.txt
+)
+
+:: SYNTAX: xcopy "SOURCE_FOLDER\*.cfg" "DESTINATION_FOLDER" /Y /I/ /Q
+:: You can also add /EXCLUDE:_exclude_list.txt        // But you need to make a file called `_exclude_list.txt` and write `.txt` and `.exe` inside it if you want to exclude all *.txt & *.exe files. Even if any file/folder which contains `.txt` & `.exe` in its name will also be excluded if it exists in SOURCE_FOLDER.
+:: /Y = Will not prompt to replace already existing file at DESTINATION_FOLDER.
+:: /I = If specified DESTINATION_FOLDER doesn't exist then it will create with the name specified in this script.
+:: /Q = Suppresses the display of xcopy messages.
+:: /S = Copies directories and subdirectories, unless they are empty. If you omit /s, xcopy works within a single directory.
+:: /E = Copies all subdirectories, even if they are empty. Use /e with the /s and /t command-line options.
+:: /T = Copies the subdirectory structure (that is, the tree) only, not files. To copy empty directories, you must include the /e command-line option.
+:: /H = Copies files with hidden and system file attributes. By default, xcopy does not copy hidden or system files.
+:: /K = Copies files and retains the read-only attribute on Destination files if present on the Source files. By default, xcopy removes the read-only attribute.
+:: xcopy DOCUMENTATION:- https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/xcopy
+:: For loop DOCUMENTATION:- https://ss64.com/nt/for.html
 
 echo -----------------
-echo Files Copied Successfully. 
+echo Files Copied Successfully.
 echo Press ENTER to exit...
-set /p input=
+set /p input=            & :: used to hold the output screen after execution of the batch file.
